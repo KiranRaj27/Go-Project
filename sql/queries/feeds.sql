3fbd0c100f7d86f48bf695b27c7c2b34c6e09afd
Kiranraj27 <kiranraj586@gmail.com>
2024-02-11 22:05:35 +0530

    Feeds added

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -1,7 +1,5 @@
--- name: CreateUser :one
-INSERT INTO users(id,created_at,updated_at,name,api_key)
-VALUES($1,$2,$3,$4,encode(sha256(random()::text::bytea),'hex'))
+-- name: CreateFeed :one
+INSERT INTO feeds(id,created_at,updated_at,name,url,user_id)
+VALUES($1,$2,$3,$4,$5,$6)
 RETURNING *;
 
--- name: GetUserByApiKey :one
-SELECT * FROM users WHERE api_key = $1;
